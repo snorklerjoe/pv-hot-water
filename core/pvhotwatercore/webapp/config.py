fbd6243c1f29fe50ce6b7098d490c8ffc4349e1f
Joseph Freeston <snorklerjoe@gmail.com>
2024-06-18 16:59:31 -0400

    Removed yet unused dotenv reference

--- core/pvhotwatercore/webapp/config.py
+++ core/pvhotwatercore/webapp/config.py
@@ -3,13 +3,13 @@
 
 import os
 from typing import Any, Dict
-from dotenv import dotenv_values
+# from dotenv import dotenv_values
 
 __all__ = ['config']
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 config: Dict[str, Any] = {
-    **dotenv_values(".env"),
+    #    **dotenv_values(".env"),
     **os.environ
 }
